f5c3180731c3307dba5a8ab1a04242816fbd6513
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-11-16 14:35:48 +0000

    updated fetch data requested

--- src/server.js
+++ src/server.js
@@ -15,6 +15,10 @@ app.use(cors())
 
 app.use(userRouter, orderRouter);
 
+app.listen(port, () => {
+    console.log(`Listening to Port ${port}`);
+})
+
 app.post('/getGames', (req, res) => {
     axios({
         url: "https://api.igdb.com/v4/games",
@@ -24,7 +28,7 @@ app.post('/getGames', (req, res) => {
             'Client-ID': '1cncw8f6iwzanjuvsr622au2lm0o17',
             "Authorization": "Bearer jy28ba3opmq24gwnuza3fd89fz9pjd"
         },
-        data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+        data: "fields name, summary;"
     })
         .then(response => {
             res.json(response.data)
@@ -43,13 +47,13 @@ app.post('/getPlatform', (req, res) => {
             'Client-ID': '1cncw8f6iwzanjuvsr622au2lm0o17',
             "Authorization": "Bearer jy28ba3opmq24gwnuza3fd89fz9pjd"
         },
-        data: "fields abbreviation,alternative_name,category,checksum,created_at,generation,name,platform_family,platform_logo,slug,summary,updated_at,url,versions,websites;"
-      })
+        data: "fields name;"
+        })
         .then(response => {
-            console.log(response.data);
+        res.json(response.data)
         })
         .catch(err => {
-            console.error(err);
+        res.send(err)
         });
     })
 
@@ -63,7 +67,7 @@ app.post('/getTopRated', (req, res) => {
             'Client-ID': '1cncw8f6iwzanjuvsr622au2lm0o17',
             "Authorization": "Bearer jy28ba3opmq24gwnuza3fd89fz9pjd"
         },
-        data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; sort rating desc;"
+        data: "fields name, rating, summary; sort rating desc;"
       })
       .then(response => {
         res.json(response.data)
@@ -82,7 +86,7 @@ app.post('/getLatest', (req, res) => {
             'Client-ID': '1cncw8f6iwzanjuvsr622au2lm0o17',
             "Authorization": "Bearer jy28ba3opmq24gwnuza3fd89fz9pjd"
         },
-        data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; sort release_dates desc;"
+        data: "fields name, release_dates, summary; sort release_dates desc;"
       })
       .then(response => {
         res.json(response.data)
@@ -92,12 +96,5 @@ app.post('/getLatest', (req, res) => {
     });
 })
 
-app.get("/health", (req, res) => {
-    res.status(200).send({ message: "API is working." });
-});
-
-
-app.listen(port, () => {
-    console.log(`Listening to Port ${port}`);
-})
-
+    
+    
